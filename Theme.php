911d31e1304f2de739f3a66c830cb6b7991e26b7
root <root@MAPACULTURAL.riogrande.local>
2019-03-25 17:39:51 -0300

    commit 1 RioGrande

--- Theme.php
+++ Theme.php
@@ -1,5 +1,6 @@
 <?php
 namespace RioGrande;
+//@import '../../../../BaseV1/assets/css/sass/main';
 use MapasCulturais\Themes\BaseV1;
 use MapasCulturais\App;
 
@@ -14,25 +15,25 @@ class Theme extends BaseV1\Theme{
         $url_search_projects = $self->searchProjectsUrl;
 
         return [
-//            'site: name' => App::i()->config['app.siteName'],
-//            'site: description' => App::i()->config['app.siteDescription'],
-//            'site: in the region' => 'na região',
-//            'site: of the region' => 'da região',
-//            'site: owner' => 'Secretaria',
-//            'site: by the site owner' => 'pela Secretaria',
-//
-//            'home: title' => "Bem-vind@!",
-//            'home: abbreviation' => "MC",
-//            'home: colabore' => "Colabore com o Mapas Culturais",
-//            'home: welcome' => "O Mapas Culturais é uma plataforma livre, gratuita e colaborativa de mapeamento cultural.",
-//            'home: events' => "Você pode pesquisar eventos culturais nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
-//            'home: agents' => "Você pode colaborar na gestão da cultura com suas próprias informações, preenchendo seu perfil de agente cultural. Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural da região. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
-//            'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.",
-//            'home: projects' => "Reúne projetos culturais ou agrupa eventos de todos os tipos. Neste espaço, você encontra leis de fomento, mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma. Cadastre-se e divulgue seus projetos.",
-//            'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o Mapas Culturais. A primeira é através da nossa <a href="https://github.com/hacklabr/mapasculturais/blob/master/documentation/docs/mc_config_api.md" target="_blank">API</a>. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Mapas Culturais é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
-//
-//            'search: verified results' => 'Resultados Verificados',
-//            'search: verified' => "Verificados"
+            'site: name' => 'Rio Grande', //App::i()->config['app.siteName'],
+            'site: description' => 'SMCULTRG', //App::i()->config['app.siteDescription'],
+            'site: in the region' => 'na região do Rio Grande',
+            'site: of the region' => 'da região do Rio Grande',
+            'site: owner' => 'Secretaria de Município da Cultura de Rio Grande',
+            'site: by the site owner' => 'pela Secretaria de Município da Cultura de Rio Grande',
+
+            'home: title' => "Bem-vind@ ao Mapa Cultural de Rio Grande!", //. App::i()->config['app.siteName'],
+            'home: abbreviation' => "SMCULTRG",
+            'home: colabore' => "Colabore com o Mapas Culturais",
+            'home: welcome' => "O Mapas Culturais é uma plataforma livre, gratuita e colaborativa de mapeamento cultural.",
+            'home: events' => "Você pode pesquisar eventos culturais nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
+            'home: agents' => "Você pode colaborar na gestão da cultura com suas próprias informações, preenchendo seu perfil de agente cultural. Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural da região. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
+            'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.",
+            'home: projects' => "Reúne projetos culturais ou agrupa eventos de todos os tipos. Neste espaço, você encontra leis de fomento, mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma. Cadastre-se e divulgue seus projetos.",
+            'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o Mapas Culturais. A primeira é através da nossa <a href="https://github.com/hacklabr/mapasculturais/blob/master/documentation/docs/mc_config_api.md" target="_blank">API</a>. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Mapas Culturais é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
+
+            'search: verified results' => 'Resultados Verificados',
+            'search: verified' => 'Verificados'
         ];
     }
 
@@ -50,6 +51,7 @@ class Theme extends BaseV1\Theme{
 
     protected function _publishAssets() {
         $this->jsObject['assets']['logo-instituicao'] = $this->asset('img/logo-instituicao.png', false);
+//	$this->jsObject['assets']['fundo'] = $this->asset('img/fundo.png', false);
     }
 
 }
